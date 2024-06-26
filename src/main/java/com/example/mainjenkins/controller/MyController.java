ddf5e7d7cc0dac234535c1cca9fa3eb7e4691f1b
摩卡君 <1296939448@qq.com>
2024-06-26 23:42:42 +0800

    org

---
+++ src/main/java/com/example/mainjenkins/controller/MyController.java
@@ -0,0 +1,12 @@
+package com.example.mainjenkins.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+    @RequestMapping("/controller")
+    public String myController(){
+        return "Hi, this is Original Info";
+    }
+}
